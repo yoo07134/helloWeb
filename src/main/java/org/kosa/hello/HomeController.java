091f7bae7e8c0c725735fdb7b8900375b756544b
yoo07134 <yoo07134@naver.com>
2025-06-11 15:55:50 +0900

    yml파일 추가

--- src/main/java/org/kosa/hello/HomeController.java
+++ src/main/java/org/kosa/hello/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
 	@GetMapping("/")
 	public String index(ServletRequest request) {
 		log.info("server port = {}", request.getServerPort());
-		return "교수님 저 진짜로 집에 가고 싶어요"+request.getServerPort();
+		return "아!!!!!!! 집에 가고싶다!!!!!!!!!!!!!"+request.getServerPort();
 	}
 
 }
