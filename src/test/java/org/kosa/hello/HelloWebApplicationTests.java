44b7f00d4ef3fbd9c7bc61555cf83d07c9213e0b
yoo07134 <yoo07134@naver.com>
2025-06-12 12:15:59 +0900

    테스트 과정 추가

--- src/test/java/org/kosa/hello/HelloWebApplicationTests.java
+++ src/test/java/org/kosa/hello/HelloWebApplicationTests.java
@@ -2,6 +2,7 @@ package org.kosa.hello;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class HelloWebApplicationTests {
@@ -9,5 +10,18 @@ class HelloWebApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	private int add(int a, int b) {
+        return a + b * 10;
+    }
+
+    @Test
+    void testAdd() {
+        int a = 10;
+        int b = 20;
+        int sum = add(a, b);
+
+        assertEquals(30, sum);
+    }
 
 }
